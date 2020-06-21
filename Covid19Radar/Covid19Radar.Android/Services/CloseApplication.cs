34148a0e1d414daaa16bd7c0e27df41ed9763d0a
Reo Ando <ando.reo420@gmail.com>
2020-06-21 21:03:13 +0900

    #500 Androidプロジェクト内のcsファイルに不要なインポート文が含まれているので削除する

--- Covid19Radar/Covid19Radar.Android/Services/CloseApplication.cs
+++ Covid19Radar/Covid19Radar.Android/Services/CloseApplication.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Covid19Radar.Droid.Services;
+﻿using Covid19Radar.Droid.Services;
 using Covid19Radar.Services;
 using Xamarin.Forms;
 
